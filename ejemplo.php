5313d09d3fb66d098d69e4dfd31daf35e8a01168
Maria Luisa <32761571+mldaw@users.noreply.github.com>
2018-05-16 16:29:26 +0200

    Update ejemplo.php

--- ejemplo.php
+++ ejemplo.php
@@ -1,10 +1,28 @@
 <?php
+/**
+* Documentacion
+* ejemplo.php
+* Clase Ejemplo con metodos
+*
+*/
 
+/**
+* Metodo multiplicarTresNumeros
+* retorna el valor de la multiplicacion.
+* @param int $v1,int $v2,int $v3
+* @return int
+*
+*/
 function multiplicarTresNumeros ($v1, $v2, $v3)
 {
 	return $v1*$v2*$v3;
 }
 
+/**
+* @author mluisa
+* Documentacion de la clase Ejemplo
+* 
+*/
 class Ejemplo {
 
 	var $a;
